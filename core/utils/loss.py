cd44ca22ba67d86f29fda5261e62d4ba65c58a71
Oluwadamilola Saka <eazydammy@gmail.com>
2020-04-26 01:07:32 -0500

    update loss

--- core/utils/loss.py
+++ core/utils/loss.py
@@ -4,14 +4,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MixSoftmaxCrossEntropyLoss(nn.CrossEntropyLoss):
-    def __init__(self, ignore_index=-1, **kwargs):
+    # def __init__(self, ignore_index=-1, **kwargs):
+    def __init__(self, ignore_index=-1):
         super(MixSoftmaxCrossEntropyLoss, self).__init__(ignore_index=ignore_index)
 
-    def forward(self, *inputs, **kwargs):
-        preds, target = tuple(inputs)
-        inputs = tuple(list(preds) + [target])
+    # def forward(self, *inputs, **kwargs):
+    def forward(self, preds, target):
+        # preds, target = tuple(inputs)
+        # inputs = tuple(list(preds) + [target])
 
-        return dict(loss=super(MixSoftmaxCrossEntropyLoss, self).forward(*inputs))
+        # return dict(loss=super(MixSoftmaxCrossEntropyLoss, self).forward(*inputs))
+        return dict(loss=super(MixSoftmaxCrossEntropyLoss, self).forward(preds, target))
 
 def get_segmentation_loss(**kwargs):
     return MixSoftmaxCrossEntropyLoss(**kwargs)
